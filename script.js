203aeba6cf128cd36b0f0ec89efa1a6a480c5a79
Jabo Jean Marie <jabojeanmarie5@gmail.com>
2023-01-05 20:15:20 +0200

    res

--- script.js
+++ script.js
@@ -42,10 +42,7 @@ form.addEventListener('submit', function(e){
 	validateForm();
 });
 
-
 var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
-
 
 function validateForm(){
 
